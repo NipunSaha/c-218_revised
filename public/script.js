cc7692072ddd56864fb220cf251b515776161d45
NipunSaha <76034504+NipunSaha@users.noreply.github.com>
2023-08-29 19:34:55 +0530

    invitation button added

--- public/script.js
+++ public/script.js
@@ -7,43 +7,49 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
-const myvideo = document.createElement("video")
-myvideo.muted = true
-var mystream
 
-navigator.mediaDevices.getUserMedia({
-    audio : true,
-    video : true
-}).then((stream)=>{
-    mystream = stream
-    addVideoStream(myvideo,mystream)
-    socket.on("user-connected",(userId)=>{
-        connectToNewUser(userId,stream)
-    })
-    peer.on("call",(call)=>{
-        call.answer(stream)
-        const video = document.createElement("video")
-        call.on("stream",(uservideostream)=>{
-            addVideoStream(video,uservideostream)
-        })
-    })
-})
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
-function connectToNewUser(userId,stream){
-    const call = peer.call(userId,stream)
-    const video = document.createElement("video")
-    call.on("stream",(uservideostream)=>{
-        addVideoStream(video,uservideostream)
-    })
-}
+let myStream;
 
-function addVideoStream(video,stream){
-    video.srcObject = stream
-    video.addEventListener("loadedmetadata",()=>{
-        video.play()
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
+    })
+
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
         $("#video_grid").append(video)
-    })
-}
+    });
+};
 
 $(function () {
     $("#show_chat").click(function () {
@@ -56,6 +62,7 @@ $(function () {
         $(".right-window").css("display", "none")
         $(".header_back").css("display", "none")
     })
+
     $("#send").click(function () {
         if ($("#chat_message").val().length !== 0) {
             socket.emit("message", $("#chat_message").val());
@@ -69,21 +76,57 @@ $(function () {
             $("#chat_message").val("");
         }
     })
-    $("#mute_button").click(function (){
-        const enabled = mystream.getAudioTracks()[0].enabled
-        if(enabled){
-            mystream.getAudioTracks()[0].enabled = false
-            html = `<i class="fas fa-microphone-slash"></i>`
-            $("#mute_button").toggleClass("background_red")
+
+    $("#mute_button").click(function () {
+        const enabled = myStream.getAudioTracks()[0].enabled;
+        if (enabled) {
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
-        }
-        else{
-            mystream.getAudioTracks()[0].enabled = true
-            html = `<i class="fas fa-microphone"></i>`
-            $("#mute_button").toggleClass("background_red")
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
         }
     })
+
+    $("#stop_video").click(function () {
+        const enabled = myStream.getVideoTracks()[0].enabled;
+        if (enabled) {
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        } else {
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
+            $("#stop_video").html(html)
+        }
+    })
+
+    $("#invite_button").click(function(){
+        const to = prompt("Enter the email address to invite...")
+        var data = {
+            url : window.location.href,
+            to : to
+        }
+        $.ajax({
+            url : "/send-mail",
+            type : "post",
+            data : JSON.stringify(data),
+            contentType : "application/json",
+            success : function(result){
+                alert("Invite Sent!")
+            },
+            error : function(error){
+                console.log(error.responseJSON)
+            }
+        })
+    })
+
 })
 
 peer.on("open", (id) => {
